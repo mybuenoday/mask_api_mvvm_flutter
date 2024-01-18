160b1d4edb6fa5ae12334743ed22d97eea319cce
mybuenoday <yoonjibiz@gmail.com>
2024-01-18 12:35:35 +0900

    feat: Update 'stores' list management in fetch function - Implemented logic to clear and then update 'stores' list with API data.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'model/store.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,26 +20,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final stores = <Store>[];
+
   Future fetch() async {
     // FormatException 오류 -> URI 생성 부분 수정
     // Uri.https 생성자를 사용할 때는 첫 번째 인자로 도메인 이름을, 두 번째 인자로 경로를 전달
     var url = Uri.https('gist.githubusercontent.com', '/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
     var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+
+    final jsonResult = jsonDecode(utf8.decode( response.bodyBytes));
+    final jsonStores = jsonResult['stores'];
+    stores.clear();
+    jsonStores.forEach((e) {
+      stores.add(Store.fromJson(e));
+    });
+
   }
 
   @override
@@ -46,7 +54,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(title: Text('title'),),
       body: Center(
         child: ElevatedButton(
-          onPressed: fetch,
+          onPressed: () async {
+            await fetch();
+            print(stores.length);
+          },
           child: Text('테스트'),
         )
       ),
