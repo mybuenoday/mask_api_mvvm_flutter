60d1621cc39c42e08439e5fd97ca44a6e7f592b4
mybuenoday <yoonjibiz@gmail.com>
2024-01-22 09:37:53 +0900

    feat: Update mask stock status display and color coding - trailing: _buildRemainStatWidget(e), switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -81,10 +82,45 @@ class _MyHomePageState extends State<MyHomePage> {
                 return ListTile(
                   title: Text(e.name ?? ''),
                   subtitle: Text(e.addr ?? ''),
-                  trailing: Text(e.remainStat ?? '매진'),
+                  trailing: _buildRemainStatWidget(e),
                 );
               }).toList(),
             ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30-99개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2-29개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+    }
+
+    return Column(
+      children: [
+        Text(remainStat, style: TextStyle(color: color, fontWeight: FontWeight.bold),),
+        Text(description),
+      ],
     );
   }
 
