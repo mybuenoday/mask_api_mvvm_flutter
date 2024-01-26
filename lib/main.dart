21a4522fb03f0752cfb51022bb95b0948fd0cb1f
mybuenoday <yoonjibiz@gmail.com>
2024-01-26 12:23:29 +0900

    refactor: Separate Model and View in MVVM structure and implement StoreRepository for data processing
    - Created StoreRepository class in the model layer for data processing.
    - MyHomePage(View) handles UI rendering, while data-related logic is separated into StoreRepository(Model).
    - Initial data loading implemented by calling fetch function of StoreRepository in initState.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
+import 'package:mask_api_mvvm/repository/store_repository.dart';
 import 'model/store.dart';
 
 void main() {
@@ -32,49 +29,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final stores = <Store>[];
+  var stores = <Store>[];
   var isLoading = true;
 
-  Future fetch() async {
-    setState(() {
-      isLoading = true;
-    });
-    var url = Uri.https('gist.githubusercontent.com',
-        '/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
-    var response = await http.get(url);
-
-    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
-    final jsonStores = jsonResult['stores'];
-
-    setState(() {
-      // 쌓인 리스트 새로고침 위한 clear
-      stores.clear();
-      jsonStores.forEach((e) {
-        stores.add(Store.fromJson(e));
-      });
-      isLoading = false;
-    });
-    print('fetch 완료');
-  }
+  final storeRepository = StoreRepository();
 
   @override
   void initState() {
     super.initState();
-    fetch();
+
+    // list stores를 리턴하는 fetch, async 대신에 .then
+    storeRepository.fetch().then((value) {
+      setState(() {
+        stores = value;
+        isLoading = false;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('마스크 구매 가능한 곳: ${stores.where((e) =>
-        e.remainStat == 'plenty' ||
-            e.remainStat == 'some' ||
-            e.remainStat == 'few').length}곳'),
+        title: Text(
+            '마스크 구매 가능한 곳: ${stores.where((e) => e.remainStat == 'plenty' || e.remainStat == 'some' || e.remainStat == 'few').length}곳'),
         actions: [
           IconButton(
             icon: const Icon(Icons.refresh),
-            onPressed: fetch,
+            onPressed: () {
+              storeRepository.fetch().then((e) {
+                (setState(() {
+                  stores = e;
+                }));
+              });
+            },
           )
         ],
       ),
---
+++ lib/repository/store_repository.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../model/store.dart';
+
+class StoreRepository {
+
+  Future<List<Store>> fetch() async {
+    final stores = <Store>[];
+/*    setState(() {
+      isLoading = true;
+    });*/
+    var url = Uri.https('gist.githubusercontent.com',
+        '/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
+    var response = await http.get(url);
+
+    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+    final jsonStores = jsonResult['stores'];
+
+    jsonStores.forEach((e) {
+      stores.add(Store.fromJson(e));
+    });
+    print('fetch 완료');
+
+    return stores;
+  }
+}
